c7b92b6bdefca4bab5cdef7a6b95eeef9175c237
Hadi <lleo8446@gmail.com>
2025-10-07 12:54:33 +0100

    initial version

---
+++ index.js
@@ -0,0 +1,22 @@
+var http = require("http");
+const port = 8000;
+
+http.createServer(function (req, res) {
+    console.log(req);
+    res.writeHead(200, { "Content-Type": "text/html" });
+    
+    res.end(`<!DOCTYPE html>
+<html>
+<head>
+    <title>MyPage</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>Welcome to My Web Page</h1>
+    <h2>About Me</h2>
+    <p>Hello! My name is Muhammad Ahmad. Im a student at Goldsmiths University, studying Computer Science.</p>
+</body>
+</html>`);
+}).listen(port, function () {
+    console.log(`Node server is running on port ${port}...`);
+});
